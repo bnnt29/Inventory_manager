7d00c89fccc3ed247a468b48d1dfb1d719e1babc
bnnt29 <65284336+bnnt29@users.noreply.github.com>
2021-06-22 20:42:49 +0200

    IM 1.6

--- server.js
+++ server.js
@@ -5,9 +5,16 @@ var db;
 var page_names = ["index", "inventory_setup", "sql_add_item", "settings"];
 var pages = ["index.html", "inventory_setup.html", "sql_add_item.html", "settings.html"];;
 
-function createDb() {
+function createDb(b) {
     console.log("createDb inventory");
-    db = new sqlite3.Database(file, createTables);
+    
+    if(b){
+        db = new sqlite3.Database(file, insertPages);
+    }else{
+        db = new sqlite3.Database(file, createTables);
+    }
+    }
+    }
 }
 
 function createTables() {
@@ -25,7 +32,7 @@ function createTables() {
     //console.log("createTable item_box");
     db.run("CREATE TABLE IF NOT EXISTS item_box (item_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL UNIQUE REFERENCES item (item_id),box_id INTEGER (100) REFERENCES box (box_id) NOT NULL,quantity INTEGER (100) NOT NULL);");
     //console.log("createTable HTML_pages");
-    db.run("CREATE TABLE IF NOT EXISTS HTML_pages (HTML_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL UNIQUE, name STRING (100) NOT NULL, path STRING (200) NOT NULL UNIQUE);", insertPages());
+    db.run("CREATE TABLE IF NOT EXISTS HTML_pages (HTML_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL UNIQUE, name STRING (100) NOT NULL, path STRING (200) NOT NULL UNIQUE);", readAllRows);
 }
 function insertPages() {
     console.log("Add Pages");
@@ -53,11 +60,11 @@ function closeDb() {
     db.close();
 }
 
-function runExample() {
-    createDb();
+function initDB(b) {
+    createDb(b);
 }
 
-runExample();
+initDB(true);
 
 var http = require('http');
 var _ = require('underscore'); var ip = _.chain(require('os').networkInterfaces()).values().flatten().filter(function (val) { return (val.family == 'IPv4' && val.internal == false) }).pluck('address').first().value();
